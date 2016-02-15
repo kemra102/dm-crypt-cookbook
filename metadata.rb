9e65bb26fdcdf8b6a452ef6730d7087f21308f29
Danny Roberts <danny.roberts@skybettingandgaming.com>
2016-02-15 22:57:58 +0000

    update metadata

--- metadata.rb
+++ metadata.rb
@@ -5,3 +5,14 @@ license 'BSD-2-Clause'
 description 'Installs/Configures dm-crypt'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version '0.1.0'
+
+%w(centos oracle redhat scientific).each do |os|
+  supports os, '>= 7.0'
+end
+supports 'ubuntu', ['= 12.04', '=14.04']
+supports 'debian', '>= 7.0'
+
+source_url 'https://github.com/kemra102/dm-crypt-cookbook' if
+  respond_to?(:source_url)
+issues_url 'https://github.com/kemra102/dm-crypt-cookbook/issues' if
+  respond_to?(:issues_url)
