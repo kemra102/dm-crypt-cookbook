22dc6497ac11a142b668543473ee6b3d0fe62c44
Chris Walker <chris.walker@skybettingandgaming.com>
2018-09-11 14:44:38 +0100

    add chef_version metadata

--- metadata.rb
+++ metadata.rb
@@ -6,6 +6,8 @@ description 'Installs/Configures dm-crypt'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version '0.1.0'
 
+chef_version '>= 12.5' if respond_to?(:chef_version)
+
 %w(centos oracle redhat scientific).each do |os|
   supports os, '>= 7.0'
 end
