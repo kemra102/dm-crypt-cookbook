51142af329650547c17d4942298e7671f833cb11
Chris Walker <chris.walker@skybettingandgaming.com>
2018-09-11 14:45:12 +0100

    minor version bump

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'danny@thefallenphoenix.net'
 license 'BSD-2-Clause'
 description 'Installs/Configures dm-crypt'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version '0.1.0'
+version '0.1.1'
 
 chef_version '>= 12.5' if respond_to?(:chef_version)
 
